efcb567aaa97eff529261299931b89329ecebb95
Jason <happyman.cg@gmail.com>
2015-01-05 16:09:26 +0800

    Added cocoapods and frameworks

---
+++ Podfile
@@ -0,0 +1,5 @@
+platform :ios, '7.0'
+
+pod 'MBProgressHUD'
+pod 'Toast'
+pod 'PPRevealSideViewController'
---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,32 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// MBProgressHUD
+#define COCOAPODS_POD_AVAILABLE_MBProgressHUD
+#define COCOAPODS_VERSION_MAJOR_MBProgressHUD 0
+#define COCOAPODS_VERSION_MINOR_MBProgressHUD 9
+#define COCOAPODS_VERSION_PATCH_MBProgressHUD 0
+
+// PPHelpMe
+#define COCOAPODS_POD_AVAILABLE_PPHelpMe
+#define COCOAPODS_VERSION_MAJOR_PPHelpMe 1
+#define COCOAPODS_VERSION_MINOR_PPHelpMe 0
+#define COCOAPODS_VERSION_PATCH_PPHelpMe 2
+
+// PPRevealSideViewController
+#define COCOAPODS_POD_AVAILABLE_PPRevealSideViewController
+#define COCOAPODS_VERSION_MAJOR_PPRevealSideViewController 1
+#define COCOAPODS_VERSION_MINOR_PPRevealSideViewController 2
+#define COCOAPODS_VERSION_PATCH_PPRevealSideViewController 0
+
+// Toast
+#define COCOAPODS_POD_AVAILABLE_Toast
+#define COCOAPODS_VERSION_MAJOR_Toast 2
+#define COCOAPODS_VERSION_MINOR_Toast 4
+#define COCOAPODS_VERSION_PATCH_Toast 0
+
